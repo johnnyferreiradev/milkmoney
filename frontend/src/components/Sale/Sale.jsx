1e7a0bada490052258fa358daabfbb68d04ce20f
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-12-06 09:37:57 -0300

    Criação da estrutura base dos itens de venda

--- frontend/src/components/Sale/Sale.jsx
+++ frontend/src/components/Sale/Sale.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import './Styles.css';
+
 export default class Sale extends React.Component {
   render() {
     return (
-      <h1>Sale 1</h1>
+      <div className="sale-item">
+        <p>Data: <span>dd/mm/aa</span></p>
+        <p>Quantidade produzido: <span>X litros</span></p>
+        <p>Quantidade vendido: <span>Y litros</span></p>
+        <p>Valor vendido: <span>X,XX R$</span></p>
+      </div>
     );
   }
 }
