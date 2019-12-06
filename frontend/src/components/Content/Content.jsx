dbadd32fb13e854f6f4b21be7a81062146d9e695
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-12-06 08:49:19 -0300

    Criação da lista de vendas e itens de venda

--- frontend/src/components/Content/Content.jsx
+++ frontend/src/components/Content/Content.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import SalesList from '../SalesList/SalesList';
+
 import './Styles.css';
 
 export default function Content () {
   return (
     <div className="content">
       <h1>Conteúdo principal</h1>
+      <SalesList />
     </div>
   );
 }
---
+++ frontend/src/components/SalesList/SalesList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Sale from '../Sale/Sale';
+
+export default function SalesList() {
+  return (
+    <>
+      <Sale />
+      <Sale />
+      <Sale />
+      <Sale />
+    </>
+  );
+}
