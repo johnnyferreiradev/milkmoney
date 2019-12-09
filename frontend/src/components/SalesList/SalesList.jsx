8e418b6fd494af713791da8df5c68d4f57cfaab4
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-12-09 20:18:15 -0200

    Criação de uma base de dados fictícia para testes

--- frontend/src/components/SalesList/SalesList.jsx
+++ frontend/src/components/SalesList/SalesList.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 
 import Sale from '../Sale/Sale';
 
-export default function SalesList() {
-  return (
-    <>
-      <Sale />
-      <Sale />
-      <Sale />
-      <Sale />
-    </>
-  );
+export default class SalesList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sales: [
+        { saleId: 1, saleDate: '01/01/2020', amountProduced: 50, soldAmount: 40},
+        { saleId: 2, saleDate: '02/02/2020', amountProduced: 40, soldAmount: 40},
+        { saleId: 3, saleDate: '03/03/2020', amountProduced: 60, soldAmount: 58},
+        { saleId: 4, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
+      ],
+      literValue: 1.50,
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Sale />
+        <Sale />
+        <Sale />
+        <Sale />
+      </>
+    );
+  }
 }
