7f500fd9ea37f39cdc56edaa19b9575b9a25c511
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-12-09 20:46:32 -0200

    Criação da listagem de vendas

--- frontend/src/components/Sale/Sale.jsx
+++ frontend/src/components/Sale/Sale.jsx
@@ -3,13 +3,23 @@ import React from 'react';
 import './Styles.css';
 
 export default class Sale extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      literValue: 1.50,
+    };
+  }
+
   render() {
+    const { saleDate, amountProduced, soldAmount} = this.props;
+    const { literValue } = this.state;
+
     return (
       <div className="sale-item">
-        <p>Data: <span>dd/mm/aa</span></p>
-        <p>Quantidade produzido: <span>X litros</span></p>
-        <p>Quantidade vendido: <span>Y litros</span></p>
-        <p>Valor vendido: <span>X,XX R$</span></p>
+        <p>Data: <span>{saleDate}</span></p>
+        <p>Quantidade produzido: <span>{amountProduced}</span></p>
+        <p>Quantidade vendido: <span>{soldAmount}</span></p>
+        <p>Valor vendido: <span>{literValue * soldAmount} R$</span></p>
       </div>
     );
   }
--- frontend/src/components/SalesList/SalesList.jsx
+++ frontend/src/components/SalesList/SalesList.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Sale from '../Sale/Sale';
 
+import './Styles.css';
+
 export default class SalesList extends React.Component {
   constructor(props) {
     super(props)
@@ -12,18 +14,28 @@ export default class SalesList extends React.Component {
         { saleId: 3, saleDate: '03/03/2020', amountProduced: 60, soldAmount: 58},
         { saleId: 4, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
       ],
-      literValue: 1.50,
     }
   }
 
   render() {
+    const sales = this.state.sales;
+    const listItems = sales.map((sale) => {
+      return (
+        <Sale
+          key={sale.id}
+          saleDate={sale.saleDate}
+          amountProduced={sale.amountProduced}
+          soldAmount={sale.soldAmount}
+        />
+      );
+    });
+
     return (
-      <>
-        <Sale />
-        <Sale />
-        <Sale />
-        <Sale />
-      </>
+      <div className="sales-list">
+        {
+          listItems
+        }
+      </div>
     );
   }
 }
