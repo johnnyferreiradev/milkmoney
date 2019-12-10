ed953c5cfacc4d7d77993cf8c1f81a97d6b39d63
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-12-09 22:29:31 -0200

    Ajuste da chave da listagem de vendas

--- frontend/src/components/SalesList/SalesList.jsx
+++ frontend/src/components/SalesList/SalesList.jsx
@@ -9,10 +9,10 @@ export default class SalesList extends React.Component {
     super(props)
     this.state = {
       sales: [
-        { saleId: 1, saleDate: '01/01/2020', amountProduced: 50, soldAmount: 40},
-        { saleId: 2, saleDate: '02/02/2020', amountProduced: 40, soldAmount: 40},
-        { saleId: 3, saleDate: '03/03/2020', amountProduced: 60, soldAmount: 58},
-        { saleId: 4, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
+        { id: 1, saleDate: '01/01/2020', amountProduced: 50, soldAmount: 40},
+        { id: 2, saleDate: '02/02/2020', amountProduced: 40, soldAmount: 40},
+        { id: 3, saleDate: '03/03/2020', amountProduced: 60, soldAmount: 58},
+        { id: 4, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
       ],
     }
   }
