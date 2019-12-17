b58825b714c1bd3b68ac7f2df3e5c14fdb7ab5e0
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2019-12-17 14:34:53 -0300

    Criação e aplicação do componente SalesForm

---
+++ frontend/src/components/SalesForm/SalesForm.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import TitlePage from '../../template/TitlePage/TitlePage';
+
+export default class SalesForm extends React.Component {
+    render() {
+        return (
+            <div className="content">
+                <TitlePage />
+                <form>
+                    <div className="form-sale-group">
+                        <label htmlFor="">Data da venda</label>
+                        <input type="date" />
+                    </div>
+                    <div className="form-sale-group">
+                        <label htmlFor="">Quantidade produzida:</label>
+                        <input type="number" step="0.001" />
+                    </div>
+                    <div className="form-sale-group">
+                        <label htmlFor="">Quantidade vendida:</label>
+                        <input type="number" step="0.001" />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
--- frontend/src/pages/SalesFormPage/SalesFormPage.jsx
+++ frontend/src/pages/SalesFormPage/SalesFormPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Nav from '../../components/Nav/Nav';
+import SalesForm from '../../components/SalesForm/SalesForm';
 
 import './Styles.css';
 
@@ -9,23 +10,7 @@ export default class FormNewSale extends React.Component {
         return (
             <div className="sales-form-page">
                 <Nav />
-                <div className="content">
-                    <h1>Nova venda</h1>
-                    <form>
-                        <div className="form-sale-group">
-                            <label htmlFor="">Data da venda</label>
-                            <input type="date"/>
-                        </div>
-                        <div className="form-sale-group">
-                            <label htmlFor="">Quantidade produzida:</label>
-                            <input type="number" step="0.001" />
-                        </div>
-                        <div className="form-sale-group">
-                            <label htmlFor="">Quantidade vendida:</label>
-                            <input type="number" step="0.001"/>
-                        </div>
-                    </form>
-                </div>
+                <SalesForm />
             </div>
         );
     }
