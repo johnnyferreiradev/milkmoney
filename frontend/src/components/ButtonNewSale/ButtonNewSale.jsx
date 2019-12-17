4fa64e33be4fff4df8eb665d0e9fd0efc7b0ef06
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2019-12-17 14:38:49 -0300

    Criação do link para pagina de cadastro de venda

--- frontend/src/components/ButtonNewSale/ButtonNewSale.jsx
+++ frontend/src/components/ButtonNewSale/ButtonNewSale.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class ButtonNewSale extends React.Component {
     render() {
         return (
             <div className="button-new-sale">
-                <button>Nova venda</button>
+                <Link to="/salesform">Nova venda</Link>
             </div>
         );
     }
