4f2e080a836e352f6c4c32fae34b69a272d354d3
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2019-12-19 14:06:14 -0300

    Criação do botão para voltar para determinada pag

---
+++ frontend/src/template/BackButton/BackButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../Styles.css';
+
+export default function BackButton(props) {
+    return (
+        <div className="back-button">
+            <Link to={props.path}> Voltar </Link>
+        </div>
+    );
+}
