5a81fef6019565f64e3ce59349ec66c7a9f6aab1
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2019-12-23 14:42:11 -0300

    Atribuição de htmlFor a labels de nova venda

--- frontend/src/components/SalesForm/SalesForm.jsx
+++ frontend/src/components/SalesForm/SalesForm.jsx
@@ -9,16 +9,16 @@ export default class SalesForm extends React.Component {
                 <TitlePage title="Nova venda" />
                 <form>
                     <div className="form-sale-group">
-                        <label htmlFor="">Data da venda</label>
-                        <input type="date" />
+                        <label htmlFor="sales-form-date">Data da venda</label>
+                        <input type="date" id="sales-form-date"/>
                     </div>
                     <div className="form-sale-group">
-                        <label htmlFor="">Quantidade produzida:</label>
-                        <input type="number" step="0.001" />
+                        <label htmlFor="sales-form-amount-produced">Quantidade produzida:</label>
+                        <input type="number" step="0.001" id="sales-form-amount-produced"/>
                     </div>
                     <div className="form-sale-group">
-                        <label htmlFor="">Quantidade vendida:</label>
-                        <input type="number" step="0.001" />
+                        <label htmlFor="sales-form-sold-amount">Quantidade vendida:</label>
+                        <input type="number" step="0.001" id="sales-form-sold-amount" />
                     </div>
                 </form>
             </div>
