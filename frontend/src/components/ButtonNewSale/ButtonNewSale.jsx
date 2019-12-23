720d442fe542aea7042ff5a1b9957a58cbf34803
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2019-12-23 16:36:33 -0300

    Criação de estilo para botão

--- frontend/src/components/ButtonNewSale/ButtonNewSale.jsx
+++ frontend/src/components/ButtonNewSale/ButtonNewSale.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Styles.css';
+
 export default class ButtonNewSale extends React.Component {
     render() {
         return (
-            <div className="button-new-sale">
-                <Link to="/salesform">Nova venda</Link>
+            <div className="btn-new-sale">
+                <Link to="/salesform" className="button-new-sale">Nova venda</Link>
             </div>
         );
     }
