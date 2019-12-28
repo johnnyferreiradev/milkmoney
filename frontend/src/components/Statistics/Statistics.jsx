609c7accf124953b0aa98ea791cadf0d1b347abf
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2019-12-28 10:23:10 -0300

    Criação do estilo da área de estatísticas mensais

--- frontend/src/components/Statistics/Statistics.jsx
+++ frontend/src/components/Statistics/Statistics.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
+import './Styles.css';
+
 export default class Statistics extends React.Component {
     render() {
         return (
             <div className="statistics">
-                <h2>Estatísticas</h2>
-                <p>Quantidade produzida este mês: Y Litros</p>
-                <p>Quantidade vendido este mês: X Litros</p>
-                <br/>
-                <h1>Total vendido este mês: Z,ZZ R$</h1>
+                <h1 className="statistics-title">Estatísticas</h1>
+                <p className="statistics-date">Janeiro de 2020</p>
+                <hr/>
+                <div className="statistics-quantities">
+                    <p><span>X</span> litros produzidos este mês</p>
+                    <p><span>Y</span> litros vendidos este mês</p>
+                </div>
+                <div className="statistics-total">
+                    <p>Total vendido este mês</p>
+                    <h2 className="total">ZZ,ZZ R$</h2>
+                </div>
             </div>
         );
     }
