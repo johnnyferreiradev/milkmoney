efe62d7cf1ede5370012f188e8c0cfe16858c316
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2019-12-28 09:39:54 -0300

    Aplicação de estilo a mensagem de erro

--- frontend/src/components/SalesList/SalesList.jsx
+++ frontend/src/components/SalesList/SalesList.jsx
@@ -10,21 +10,21 @@ export default class SalesList extends React.Component {
     super(props)
     this.state = {
       sales: [
-        { id: 1, saleDate: '01/01/2020', amountProduced: 50, soldAmount: 40},
-        { id: 2, saleDate: '02/02/2020', amountProduced: 40, soldAmount: 40},
-        { id: 3, saleDate: '03/03/2020', amountProduced: 60, soldAmount: 58},
-        { id: 4, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
-        { id: 5, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
-        { id: 6, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
-        { id: 7, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
-        { id: 8, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
+        // { id: 1, saleDate: '01/01/2020', amountProduced: 50, soldAmount: 40},
+        // { id: 2, saleDate: '02/02/2020', amountProduced: 40, soldAmount: 40},
+        // { id: 3, saleDate: '03/03/2020', amountProduced: 60, soldAmount: 58},
+        // { id: 4, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
+        // { id: 5, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
+        // { id: 6, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
+        // { id: 7, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
+        // { id: 8, saleDate: '04/04/2020', amountProduced: 52, soldAmount: 48},
       ],
     }
   }
 
   render() {
     const sales = this.state.sales;
-    const errorMessage = <p className="sale-list-error">Não há vendas registradas nesta data</p>;
+    const errorMessage = <p className="sales-list-error">Ops! Não há vendas registradas nesta data.</p>;
     const listItems = sales.map((sale) => {
       return (
         <Sale
